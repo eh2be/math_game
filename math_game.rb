e5410048238a31236d7e66adcb187ad401d3b606
eh2be <eh2be@users.noreply.github.com>
2014-10-03 18:44:55 +0000

    interim

--- math_game.rb
+++ math_game.rb
@@ -4,23 +4,28 @@ require './methods'
 class Player
   attr_accessor :name
 
-  def initialize(name)
+  def initialize(name, points, lives)
     @name = name
+    @points = 0
+    @lives = 3
   end
 
   def earn_point
+    points += 1
   end
 
   def lose_life
+    lives -= 1
   end
 
 end
 
-# Intitalize the lives
-pl_1_lives = 3
-pl_2_lives = 3
-pl_1_pts = 0
-pl_2_pts = 0
+## Don't need this in oo version
+## Intitalize the lives
+# pl_1_lives = 3
+# pl_2_lives = 3
+# pl_1_pts = 0
+# pl_2_pts = 0
 
 puts "Player 1, please enter your name: "
 name = gets.chomp
@@ -34,16 +39,16 @@ puts "Welcome #{player_2.name}, you are Player 2."
 
 
 # Loop until one player runs out of lives
-until pl_1_lives == 0 || pl_2_lives == 0 do
+until player_1.lives == 0 || player_2.lives == 0 do
   
   print "Player 1: "
   if play_turn(pl_1_lives, pl_1_pts) == true
-    pl_1_pts += 1
+    player_1.earn_point
     puts "You have #{pl_1_pts} points."
   elsif play_turn(pl_1_lives, pl_1_pts) == false # Both calls and evaluates the math question for Player 1
-    pl_1_lives -= 1
-    puts "Player 1, you have #{pl_1_pts} points and #{pl_1_lives} lives left."
-    puts "Player 2, you have #{pl_2_pts} points and #{pl_2_lives} lives left."
+    player_1.lose_life
+    puts "Player 1, you have #{player_1.points} points and #{player_1.lives} lives left."
+    puts "Player 2, you have #{player_2.points} points and #{player} lives left."
     if pl_1_lives == 0
       puts "Player 2 wins!"
       return
